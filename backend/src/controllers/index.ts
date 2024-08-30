e56f07eec661b88c2758c6291c354667ef800343
Terma <termafaxplay@outlook.com>
2024-08-29 23:57:35 -0300

    index for controllers

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,9 @@
+import { signIn } from "./auth.controller";
+import { createSuggestion } from "./suggestions.controller";
+import { getUsers } from "./users.controller";
+
+export {
+    signIn,
+    createSuggestion,
+    getUsers
+}
