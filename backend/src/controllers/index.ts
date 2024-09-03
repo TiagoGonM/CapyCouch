0a628d78ae62e7c0de6dc0abea87300e9e4ba821
Terma <termafaxplay@outlook.com>
2024-09-03 00:35:35 -0300

    useless

--- backend/src/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-import { signIn } from "./auth.controller";
-import { createSuggestion } from "./suggestions.controller";
-import { getUsers } from "./users.controller";
-
-export {
-    signIn,
-    createSuggestion,
-    getUsers
-}
