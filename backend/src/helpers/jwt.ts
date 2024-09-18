2c264e62be4ecae06c4df65398cb6ed73da95354
Terma <termafaxplay@outlook.com>
2024-09-17 22:48:49 -0300

    unused func

--- backend/src/helpers/jwt.ts
+++ backend/src/helpers/jwt.ts
@@ -1,5 +1,4 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
-import prisma from "../db/prisma";
+import jwt from "jsonwebtoken";
 
 export const generateJWT = (uid: string, name: string) => {
   return new Promise((resolve, reject) => {
@@ -23,29 +22,4 @@ export const generateJWT = (uid: string, name: string) => {
       }
     );
   });
-};
-
-export const checkJWT = async (token: any) => {
-  try {
-    if (token.length < 10) {
-      return null;
-    }
-
-    const payload = jwt.verify(token, process.env.SECRET_KEY as string);
-
-    console.log(payload);
-    // const user = await prisma.user.findUnique({where: {id: jw.}});
-
-    // if (user) {
-    //   if (!user.status) {
-    //     return null;
-    //   }
-    //   return user;
-    // }
-    return null;
-  } catch (err) {
-    console.log("Error");
-
-    return null;
-  }
 };
