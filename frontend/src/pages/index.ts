1bf22e9f139e077152209f40f649ebc64adbf233
FaustiBar <faustino.barbuto@gmail.com>
2024-10-24 17:36:25 -0300

    Pages Delete
    Eliminadas paginas "groupsPage" y "ProfilePage"

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,6 +2,4 @@ export * as HomePage from './HomePage';
 export * as LandingPage from "./LandingPage";
 export * as LoginPage from "./LoginPage";
 export * as RegisterPage from './RegisterPage';
-export * as ProfilePage from './ProfilePage';
-export * as GroupsPage from './GroupsPage';
 export * as GenrePage from './GenrePage';
